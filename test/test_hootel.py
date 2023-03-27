1b07a1b7aa496cb96f5f5e64e41937986d2b386b
Máté Gergely <105539620+mategeri@users.noreply.github.com>
2023-03-27 22:41:43 +0200

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -1,40 +1,54 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+# This workflow will install Python dependencies, run tests and lint with a single version of Python
+# For more information see: https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-python
 
-class TestHotel(object):
-    def setup_method(self):
-        service = Service(executable_path=ChromeDriverManager().install())
-        options = Options()
-        options.add_experimental_option("detach", True)
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        self.browser = webdriver.Chrome(service=service, options=options)
-        URL = "http://hotel-v2.progmasters.hu/"
-        self.browser.get(URL)
-        self.browser.maximize_window()
+name: Hotel application
 
-    def teardown_method(self):
-        self.browser.quit()
+on:
+  push:
+    branches: [ "master" ]
+  pull_request:
+    branches: [ "master" ]
 
-    def test_open(self):
-        page_name = self.browser.find_element(By.ID, 'nav-index')
-        assert page_name.is_displayed()
-        assert page_name.text == "HOOTEL"
+permissions:
+  contents: write
 
-    def test_login(self):
-        signin_btn = self.browser.find_element(By.CSS_SELECTOR, 'a[class="nav-link"]')
-        signin_btn.click()
-        email_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.ID, 'email')))
-        password_input = self.browser.find_element(By.ID, 'password')
-        login_btn = self.browser.find_element(By.CSS_SELECTOR, 'button[name="submit"]')
-        email_input.send_keys('andi.teszt2021@gmail.com')
-        password_input.send_keys('tesztelek2021')
-        login_btn.click()
-        profile_id = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.ID, 'profile')))
-        assert profile_id.text == "Profilom (Andrea)"
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@v3
+    - name: Set up Python 3.10
+      uses: actions/setup-python@v4
+      with:
+        python-version: "3.10"
+    - name: Install dependencies
+      run: |
+        python -m pip install --upgrade pip
+        pip install flake8 pytest
+        if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+    - name: Lint with flake8
+      run: |
+        # stop the build if there are Python syntax errors or undefined names
+        flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+        # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
+        flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+    - name: Test with pytest
+      run: |
+        pytest --alluredir=allure-results
+    - name: Allure Report action from marketplace
+      uses: simple-elf/allure-report-action@master
+      if: always()
+      with:
+        allure_results: allure-results
+        allure_history: allure-history
+        keep_reports: 20
+
+    - name: Deploy report to Github Pages
+      if: always()
+      uses: peaceiris/actions-gh-pages@v2
+      env:
+        PERSONAL_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        PUBLISH_BRANCH: gh-pages
+        PUBLISH_DIR: allure-history
